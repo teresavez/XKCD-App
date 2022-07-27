51f9ac66653173d6a9c3f1b8a0857186dec8a1e9
teresavez <teryvez@gmail.com>
2022-07-27 17:51:06 +0200

    Initial Commit

---
+++ XKCD App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XKCD App
+//
+//  Created by Tery Vezzuto on 27/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XKCD App/XKCD_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XKCD_AppApp.swift
+//  XKCD App
+//
+//  Created by Tery Vezzuto on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct XKCD_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
